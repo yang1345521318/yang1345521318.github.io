b714315823f71118b21117985b35e62f93d0a9af
yang1345521318 <1345521318@qq.com>
2019-10-11 18:46:45 +0800

    Site updated: 2019-10-11 18:46:43

---
+++ dist/music.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    //mini: true,
+    autoplay: false,
+    audio: [
+      {
+          name: "����˵������",
+          artist: '�ܽ���',
+          url: 'http://www.ytmp3.cn/down/69836.mp3',
+          cover: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2152746645,3129408080&fm=26&gp=0.jpg',
+      }
+    ]
+});
